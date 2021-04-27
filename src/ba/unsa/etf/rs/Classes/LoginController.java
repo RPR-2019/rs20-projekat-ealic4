fbb3475f062b4080b5fb1e89e0a3c1584c4ee6cf
ealic5 <ealic4@etf.unsa.ba>
2021-04-27 23:25:59 +0200

    Created initial form

---
+++ src/ba/unsa/etf/rs/Classes/LoginController.java
@@ -0,0 +1,53 @@
+package ba.unsa.etf.rs.Classes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+
+public class LoginController {
+
+    public Button cancel;
+
+    public void loginDoctor(ActionEvent actionEvent) throws Exception {
+
+            Stage stageLogDoctor = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/doctorLogin.fxml"));
+            stageLogDoctor.setTitle("Login doctor");
+            stageLogDoctor.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+            stageLogDoctor.show();
+
+    }
+
+    public void loginMedical(ActionEvent actionEvent) throws Exception{
+        Stage stageLogDoctor = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/medicalLogin.fxml"));
+        stageLogDoctor.setTitle("Login doctor");
+        stageLogDoctor.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        stageLogDoctor.show();
+    }
+
+    public void loginManger(ActionEvent actionEvent)  throws Exception{
+        Stage stageLogDoctor = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/managerLogin.fxml"));
+        stageLogDoctor.setTitle("Login doctor");
+        stageLogDoctor.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        stageLogDoctor.show();
+    }
+
+    public void closeWindow(ActionEvent actionEvent) {
+
+        Stage stage = (Stage)cancel.getScene().getWindow();
+        stage.close();
+    }
+
+
+}
---
+++ src/ba/unsa/etf/rs/Classes/Main.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rs.Classes;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+import static javafx.scene.control.PopupControl.USE_PREF_SIZE;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
+        primaryStage.setTitle("Home");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinWidth(750);
+        primaryStage.setMinHeight(720);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
