84e98d57fe77862be7ed159a9f683e55b632c8c9
ealic4 <ealic4@etf.unsa.ba>
2021-06-28 20:37:24 +0200

    add function button end

--- src/ba/unsa/etf/rs/Controllers/PatientController.java
+++ src/ba/unsa/etf/rs/Controllers/PatientController.java
@@ -2,10 +2,17 @@ package ba.unsa.etf.rs.Controllers;
 
 import ba.unsa.etf.rs.beans.Patient;
 import ba.unsa.etf.rs.dao.ClinicDAO;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.LocalDate;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class PatientController {
     ClinicDAO clinicDAO = new ClinicDAO();
@@ -155,6 +162,20 @@ public class PatientController {
         else if(znak == '9') broj += 9;
 
 
+    }
+
+    public void end() throws IOException {
+        Stage stage = (Stage)namePatient.getScene().getWindow();
+        stage.close();
+
+        stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/doctorFXML/doctorWork.fxml"));
+        DoctorWorkController doctorWorkController = new DoctorWorkController();
+        loader.setController(doctorWorkController);
+        Parent root = loader.load();
+        stage.setTitle("Doctor work");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.show();
     }
 
 
